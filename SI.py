27b8c319679474e45e2fabf5a101825802b399df
LucasBraz <lucasbz69@gmail.com>
2020-01-13 23:49:36 +0100

    Update SI.py

--- SI.py
+++ SI.py
@@ -3,7 +3,7 @@ import tkinter as tk
 import random as rd
 
 class Ennemi:
-    def __init__(self, fcanvas, jeu, Horde, positionx=6, positiony=1, direction = 1, vitesse = 50, frequence = 500, kind=1):
+    def __init__(self, fcanvas, jeu, Horde, positionx=6, positiony=1, direction = 1, vitesse = 2, frequence = 16, kind=1):
         self.kind = kind # Pas encore implementé
         self.fcanvas = fcanvas
         self.positionx = positionx
@@ -29,7 +29,7 @@ class Ennemi:
         self.fcanvas.after(self.frequence,self.deplacement)
 
 class Horde:
-    def __init__(self,fcanvas, jeu, length, heigth, vitesse = 50, direction = 1, frequence = 500):
+    def __init__(self,fcanvas, jeu, length, heigth, vitesse = 1, direction = 1, frequence = 16):
         self.listeEnnemis = []
         self.length = length
         self.heigth = heigth
@@ -57,14 +57,14 @@ class Horde:
                 self.fcanvas.move(Ennemi.sprite, self.direction*self.vitesse, 0)
         else:
             for Ennemi in self.listeEnnemis:
-                if self.fcanvas.coords(Ennemi.sprite)[1] + 50 + 50 > 550:
+                if self.fcanvas.coords(Ennemi.sprite)[1] + 50 + 25 > 550:
                     self.jeu.GameOver = True
             if self.jeu.GameOver:
                 self.jeu.endGame()
                 return
 
             for Ennemi in self.listeEnnemis:
-                self.fcanvas.move(Ennemi.sprite, 0, 50)
+                self.fcanvas.move(Ennemi.sprite, 0, 25)
                 Ennemi.direction = (Ennemi.direction == -1) - (Ennemi.direction == 1) # Pas utilisé dans horde mais fait par rigueur
             self.direction = (self.direction == -1) - (self.direction == 1)
         self.fcanvas.after(self.frequence,self.deplacements)
@@ -82,11 +82,11 @@ class Horde:
             
 class Tir:
     def __init__(self, fcanvas, jeu, positionx, positiony, direction):
-        self.image = tk.PhotoImage(file='laser.png')
+        self.image = [tk.PhotoImage(file='laser.png'), tk.PhotoImage(file='laser2.png')]
         self.fcanvas= fcanvas
         self.jeu = jeu
         self.direction = direction
-        self.sprite = fcanvas.create_image(positionx,positiony+direction*30,image=self.image, anchor='nw')
+        self.sprite = fcanvas.create_image(positionx,positiony+direction*30,image=self.image[(self.direction == -1)], anchor='nw')
 
 
 
@@ -98,6 +98,7 @@ class Player:
         self.sprite= fcanvas.create_image(positionx,positiony,image=self.image, anchor='nw')
         self.direction = direction
         self.jeu = jeu
+        self.TimerTir = 1000
 
     def moveleft(self, event):
         self.direction=-1
@@ -118,13 +119,24 @@ class Player:
         self.fcanvas.after(16,self.deplacementplayer)
 
     def NouveauTirP(self, event):
-        self.jeu.Tirsactuels.append(Tir(self.fcanvas, self.jeu, self.fcanvas.coords(self.sprite)[0], self.fcanvas.coords(self.sprite)[1], -1))
+        if self.TimerTir >= 50:
+            self.jeu.Tirsactuels.append(Tir(self.fcanvas, self.jeu, self.fcanvas.coords(self.sprite)[0], self.fcanvas.coords(self.sprite)[1], -1))
+            self.TimerTir = 0
+    
+    def PasdeTir(self):
+        self.TimerTir += 1
+        self.fcanvas.after(16, self.PasdeTir)
 
 class Jeu:
-    def __init__(self, fenetre):
+    def __init__(self):
         # A ameliorer pour configurer depuis la declaration d'un objet jeu
-        self.fenetre = fenetre
-        self.canvas = tk.Canvas(fenetre, bg = 'black', bd= 0, highlightthickness=0, height = 600, width = 600)
+        self.fenetre = tk.Tk()
+        self.fenetre.title('Space Invaders')
+        self.fenetre.geometry("600x600")
+        self.fenetre.resizable(width=False, height=False)
+
+        self.canvas = tk.Canvas(self.fenetre, bg = 'black', bd= 0, highlightthickness=0, height = 600, width = 600)
+
         self.player = Player(self.canvas, self)
         self.horde = Horde(self.canvas, self, 6, 3)
         self.GameOver = False
@@ -135,7 +147,8 @@ class Jeu:
         self.canvas.after(16, self.horde.deplacements)
         self.canvas.after(16, self.player.deplacementplayer)
         self.canvas.after(16, self.GestionTirs)
-        self.canvas.after(16, self.horde.NouveauTir)
+        self.canvas.after(16, self.horde.NouveauTir)       
+        self.canvas.after(16, self.player.PasdeTir)
         self.fenetre.bind('<q>', self.player.moveleft)
         self.fenetre.bind('<Q>', self.player.moveleft)
         self.fenetre.bind('<d>', self.player.moveright)
@@ -147,30 +160,35 @@ class Jeu:
         # A refaire en supprimant totalement le canvas et ses binds pour le recréer avec une fonction
         self.canvas.delete('all')
         widget = tk.Label(self.canvas, text='Game Over', fg='white', bg='black')
-        widget.pack()
-        self.canvas.create_window(300, 300, window=widget)
+        widgetc = self.canvas.create_window(300, 300, window=widget)
+    def reload(self):
+        # Visiblement à revoir
+        self.canvas = tk.Canvas(self.fenetre, bg = 'black', bd= 0, highlightthickness=0, height = 600, width = 600)
+        self.player = Player(self.canvas, self)
+        self.horde = Horde(self.canvas, self, 6, 3)
+        self.GameOver = False
+        self.Tirsactuels = []
+        self.Debut()
+
     
+
     def GestionTirs(self):
         if self.GameOver:
             return
         for tir in self.Tirsactuels:
-            self.canvas.move(tir.sprite, 0,tir.direction*5)
-            if self.canvas.coords(tir.sprite)[1] > 600 or self.canvas.coords(tir.sprite)[1] < -50:
+            self.canvas.move(tir.sprite, 0,tir.direction)
+            if abs(self.canvas.coords(tir.sprite)[0] - self.canvas.coords(self.player.sprite)[0]) >= 20 and abs(self.canvas.coords(tir.sprite)[0] - self.canvas.coords(self.player.sprite)[0]) <= 30 and tir.direction == 1 and abs(self.canvas.coords(tir.sprite)[1] - self.canvas.coords(self.player.sprite)[1]) >= 10 and abs(self.canvas.coords(tir.sprite)[1] - self.canvas.coords(self.player.sprite)[1]) <= 40:
+                print(" player touché !")
+            if self.canvas.coords(tir.sprite)[1] >= 560 or self.canvas.coords(tir.sprite)[1] < 0:
                 self.canvas.delete(tir.sprite)
                 self.Tirsactuels.remove(tir)
-        self.canvas.after(64, self.GestionTirs)
+            
+        self.canvas.after(16, self.GestionTirs)
 
 
 # Gestion mort player ou monstre + replay + obstacle
     
 if __name__ == '__main__':
-    fenetre_root = tk.Tk()
-    fenetre_root.title('Space Invaders')
-    fenetre_root.geometry ("600x600")
-    fenetre_root.resizable(width=False, height=False)
-
-    jeu = Jeu(fenetre_root)
+    jeu = Jeu()
     jeu.Debut()
-
-
-    fenetre_root.mainloop()
+    jeu.fenetre.mainloop()
