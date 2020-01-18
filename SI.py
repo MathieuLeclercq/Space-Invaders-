7ea0205a6df3ff85ffee05f0dcc4d2729d2a3636
MathieuLeclercq <56450839+MathieuLeclercq@users.noreply.github.com>
2020-01-18 11:41:38 +0100

    Update SI.py

--- SI.py
+++ SI.py
@@ -20,7 +20,7 @@ class Ennemi:
     def deplacement(self):
         if self.jeu.GameOver:
             return
-        # Deplacement pour les solitaires persistant qui n'avancent pas (boss ?)
+        # Deplacement pour les solitaires persistant500 qui n'avancent pas (boss ?)
         if self.fcanvas.coords(self.sprite)[0] + self.direction*self.vitesse > 0 and self.fcanvas.coords(self.sprite)[0] + self.direction*self.vitesse + 50 < 600:
             self.fcanvas.move(self.sprite, self.direction*self.vitesse, 0)
         else:
@@ -73,10 +73,10 @@ class Horde:
         if self.jeu.GameOver:
             return
         for Ennemi in self.listeEnnemis:
-            probatir = rd.randint(0,100)
-            if probatir <= 20:
+            probatir = rd.randint(0,5000)
+            if probatir <= 1:
                 self.jeu.Tirsactuels.append(Tir(self.fcanvas, self.jeu, self.fcanvas.coords(Ennemi.sprite)[0], self.fcanvas.coords(Ennemi.sprite)[1], 1))
-        self.fcanvas.after(5000, self.NouveauTir)
+        self.fcanvas.after(16, self.NouveauTir)
                 
                 
             
@@ -99,16 +99,26 @@ class Player:
         self.direction = direction
         self.jeu = jeu
         self.TimerTir = 1000
+        self.tir = 0
 
     def moveleft(self, event):
+        if self.jeu.GameOver:
+            return
         self.direction=-1
 
     def moveright(self, event):
+        if self.jeu.GameOver:
+            return
         self.direction=1
 
     def stopmove(self, event):
+        if self.jeu.GameOver:
+            return
         if (event.keysym == "q" and self.direction == -1) or (event.keysym == "d" and self.direction == 1):
             self.direction = 0
+        if (event.keysym == "space" and self.tir == 1):
+            self.tir = 0
+            print("tu n'appuye plus sur espace")
     def deplacementplayer(self):
         if self.jeu.GameOver:
             return
@@ -117,14 +127,22 @@ class Player:
         else:
             self.fcanvas.move(self.sprite, self.direction*5,0)
         self.fcanvas.after(16,self.deplacementplayer)
-
-    def NouveauTirP(self, event):
-        if self.TimerTir >= 50:
+        
+        if self.tir == 1 and self.TimerTir >= 25:
             self.jeu.Tirsactuels.append(Tir(self.fcanvas, self.jeu, self.fcanvas.coords(self.sprite)[0], self.fcanvas.coords(self.sprite)[1], -1))
             self.TimerTir = 0
+
+    def NouveauTirP(self, event):
+        if self.jeu.GameOver:
+            return
+        if self.TimerTir >= 25:
+            self.tir = 1  
     
     def PasdeTir(self):
+        if self.jeu.GameOver:
+            return
         self.TimerTir += 1
+
         self.fcanvas.after(16, self.PasdeTir)
 
 
@@ -146,13 +164,14 @@ class Jeu:
         self.canvas = tk.Canvas(self.fenetre, bg = 'black', bd= 0, highlightthickness=0, height = 600, width = 600)
 
         self.player = Player(self.canvas, self)
-        self.horde = Horde(self.canvas, self, 6, 3)
+        self.horde = Horde(self.canvas, self, 11, 11)
+        self.murs = Murs(self.canvas,self)
         self.GameOver = False
         self.Tirsactuels = []
 
         self.menu = Menu()
-        self.boutonPlay = tk.Button(self.fenetre, text = 'PLAY !',height = 4, width = 20,command=self.Debut,activebackground='#FFE213',background='#FFE213')
-        self.boutonExit = tk.Button(self.fenetre, text = 'EXIT',height = 2, width = 10,command=self.fenetre.destroy,activebackground='#FFE213',background='#FFE213')
+        self.boutonPlay = tk.Button(self.fenetre, text = 'PLAY !',height = 4, width = 20,command=self.Debut,activebackground='#ffbd33',background='#FFE213')
+        self.boutonExit = tk.Button(self.fenetre, text = 'EXIT',height = 2, width = 10,command=self.fenetre.destroy,activebackground='#ffbd33',background='#FFE213')
 
     def lancerMenu(self):
         self.canvas.pack(anchor='nw')
@@ -175,37 +194,101 @@ class Jeu:
         self.fenetre.bind('<D>', self.player.moveright)
         self.fenetre.bind('<KeyRelease>', self.player.stopmove)
         self.fenetre.bind('<space>', self.player.NouveauTirP)
+        
 
     def endGame(self):
         # A refaire en supprimant totalement le canvas et ses binds pour le recréer avec une fonction
         self.canvas.delete('all')
-        widget = tk.Label(self.canvas, text='Game Over', fg='white', bg='black')
+        widget = tk.Button(self.canvas, text='Game Over', fg='white', bg='black', command=self.relaunch)
         widgetc = self.canvas.create_window(300, 300, window=widget)
-    def reload(self):
+        
+    def relaunch(self):
+        self.canvas.delete('all')
         # Visiblement à revoir
         self.canvas = tk.Canvas(self.fenetre, bg = 'black', bd= 0, highlightthickness=0, height = 600, width = 600)
         self.player = Player(self.canvas, self)
-        self.horde = Horde(self.canvas, self, 6, 3)
+        self.horde = Horde(self.canvas, self, 10, 10)
+        self.murs = Murs(self.canvas,self)
         self.GameOver = False
         self.Tirsactuels = []
-        self.Debut()
-
-    
+        
+        
+        
+        self.canvas.pack(anchor='nw')
+        self.canvas.after(16, self.horde.deplacements)
+        self.canvas.after(16, self.player.deplacementplayer)
+        self.canvas.after(16, self.GestionTirs)
+        self.canvas.after(16, self.horde.NouveauTir)       
+        self.canvas.after(16, self.player.PasdeTir)
+        self.fenetre.bind('<q>', self.player.moveleft)
+        self.fenetre.bind('<Q>', self.player.moveleft)
+        self.fenetre.bind('<d>', self.player.moveright)
+        self.fenetre.bind('<D>', self.player.moveright)
+        self.fenetre.bind('<KeyRelease>', self.player.stopmove)
+        self.fenetre.bind('<space>', self.player.NouveauTirP)
 
     def GestionTirs(self):
         if self.GameOver:
             return
         for tir in self.Tirsactuels:
-            self.canvas.move(tir.sprite, 0,tir.direction)
-            if abs(self.canvas.coords(tir.sprite)[0] - self.canvas.coords(self.player.sprite)[0]) >= 20 and abs(self.canvas.coords(tir.sprite)[0] - self.canvas.coords(self.player.sprite)[0]) <= 30 and tir.direction == 1 and abs(self.canvas.coords(tir.sprite)[1] - self.canvas.coords(self.player.sprite)[1]) >= 10 and abs(self.canvas.coords(tir.sprite)[1] - self.canvas.coords(self.player.sprite)[1]) <= 40:
-                print(" player touché !")
-            if self.canvas.coords(tir.sprite)[1] >= 560 or self.canvas.coords(tir.sprite)[1] < 0:
+            touche = False
+            indexEnnemiasuppr = None 
+            indexblocasuppr = None
+            self.canvas.move(tir.sprite, 0,tir.direction*4)
+            if abs(self.canvas.coords(tir.sprite)[0] - self.canvas.coords(self.player.sprite)[0]) < 20 and abs(self.canvas.coords(tir.sprite)[1] - self.canvas.coords(self.player.sprite)[1]) < 33 and tir.direction == 1:
+                
+                touche = True
+                # TUER PLAYER ET GAME OVER
+                
+            for Ennemi in self.horde.listeEnnemis:
+                if abs(self.canvas.coords(tir.sprite)[0] - self.canvas.coords(Ennemi.sprite)[0]) < 23 and abs(self.canvas.coords(tir.sprite)[1] - self.canvas.coords(Ennemi.sprite)[1]) < 33 and tir.direction == -1:
+                    
+                    touche = True                    
+                    indexEnnemiasuppr = self.horde.listeEnnemis.index(Ennemi)                  
+                    
+            for bloc in self.murs.listeBlocs:
+                if abs(self.canvas.coords(tir.sprite)[0] - self.canvas.coords(bloc.sprite)[0]) < 25 and abs(self.canvas.coords(tir.sprite)[1] - self.canvas.coords(bloc.sprite)[1]) < 39:
+                    
+                    touche = True
+                    indexblocasuppr = self.murs.listeBlocs.index(bloc)
+            
+            
+            if indexEnnemiasuppr != None:
+                self.canvas.delete(self.horde.listeEnnemis[indexEnnemiasuppr].sprite)
+                self.horde.listeEnnemis.remove(self.horde.listeEnnemis[indexEnnemiasuppr])
+            if indexblocasuppr != None:
+                self.canvas.delete(self.murs.listeBlocs[indexblocasuppr].sprite)
+                self.murs.listeBlocs.remove(self.murs.listeBlocs[indexblocasuppr])
+                
+            
+            if self.canvas.coords(tir.sprite)[1] >= 560 or self.canvas.coords(tir.sprite)[1] < 0 or touche == True:
                 self.canvas.delete(tir.sprite)
                 self.Tirsactuels.remove(tir)
             
+            
+            
         self.canvas.after(16, self.GestionTirs)
 
-
+class Bloc:
+    def __init__(self, canvas, jeu,positionx, positiony):
+        self.image = tk.PhotoImage(file='bloc.png').subsample(25,25)
+        self.jeu = jeu
+        self.canvas = canvas
+        self.sprite = self.canvas.create_image(positionx,positiony,image=self.image, anchor='nw')
+        
+class Murs:
+    def __init__(self, fcanvas, jeu):
+        self.jeu = jeu
+        self.canvas = fcanvas
+        self.listeBlocs=[]
+        for i in range(5):
+            for j in range(3):
+                self.listeBlocs.append(Bloc(fcanvas, jeu, 60+20*i, j*20+525-40))
+                self.listeBlocs.append(Bloc(fcanvas, jeu, 260+20*i, j*20+525-40))
+                self.listeBlocs.append(Bloc(fcanvas, jeu, 460+20*i, j*20+525-40))
+                
+        
+        
 # Gestion mort player ou monstre + replay + obstacle
     
 if __name__ == '__main__':
